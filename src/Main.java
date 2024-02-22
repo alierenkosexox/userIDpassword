dbd1c673fd6eae7658ab3eae479a8013fa74a68e
Ali Eren KÖSE <alierenkose@gmail.com>
2024-02-22 04:05:28 +0300

    bug fixed and control

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,62 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {                   //Kullanıcı Adı : patika      Şifre : java123
+    public static void main(String[] args) {
+
+        String userName, userPassword, passwordReset, newpassword , resetpassword=("");
+        //kullanılacak bilgileri tanımlama
+        Scanner input = new Scanner(System.in);
+        //kullanıcı adı ve şifreyi isteyip kaydetme
+        System.out.print("Kullanıcı Adını Girin : ");
+        userName = input.nextLine();
+
+        System.out.print("Şifrenizi Girin : ");
+        userPassword = input.nextLine();
+        // eğer kullanıcı adı ve şifre doğru ise sisteme giriş başarılı
+        if (userName.equals("patika") && userPassword.equals("java123")) {
+
+            System.out.println("\nBilgiler Doğru Giriş Yapılıyor..");
+
+            //bilgiler yanlış ise hata infosu ve şifre sıfırlama sorusu sor
+        } else {
+            System.out.println("Giriş Yaptığınız Bilgiler Hatalı");
+            System.out.println("Şifrenizi Sıfırlamak İster misinz ? (evet/hayır)");
+            // şifre sıfırlama sorusunun cevabını kaydet ve yeni şifreyi input ile değiştir
+            Scanner inputReset = new Scanner(System.in);
+            passwordReset = inputReset.nextLine();
+            // şifre sıfırlama talebinin durumuna göre şifre değişme case'lerine yönlendir
+            switch (passwordReset) {
+                case "evet":
+
+                    // yeni şifreyi iste ve kaydet
+                    System.out.println("Yeni Şifrenizi Belirleyiniz :");
+                    Scanner changerinput = new Scanner(System.in);
+                    newpassword = changerinput.nextLine();
+
+                    //eski veya hatalı şifre harici girilen yeni şifeleri kabul et
+                    if (newpassword.equals(resetpassword))
+                    {
+                        System.out.print("Şifreniz başarıyla değişti");
+                    }
+                    //eğer yeni şifre eski şifre ile aynı ise hata infosu yazdır
+                    else if (newpassword.equals("java123")) {
+                        System.out.println("Yeni şifreniz Eski şifreniz ile aynı olamaz");
+
+                    //eğer yeni şifre girilen hatalı şifre ile aynı ise hata infosu yazdır
+                    } else if (newpassword.equals(userPassword)) {
+                        System.out.println("Yeni şifre Hatalı girdiğiniz şifre ile aynı olamaz.");
+                }
+                    break;      //case evet döngüsünü durdur
+
+                case "hayır":   // şifre değiştirme talebi hayır ise red mesajı yazdır
+
+                    System.out.println("Şifre değiştirmeyi onaylamadınız..");
+                    break;
+
+            }
+
         }
+
+
     }
-}
+}  // Ali Eren KÖSE 22/02/2024  03:48
